ecc14084074ff16ff1e45f36bd8a8eb9786d3e15
chendianbuji <chendianbuji@gmail.com>
2022-01-16 06:20:34 +0800

    update abc235_c

--- abc235/abc235_c.hs
+++ abc235/abc235_c.hs
@@ -5,14 +5,13 @@ main = do
   aStr <- getLine
   let a = map read (words aStr) :: [ Int ]
   pairs <- load q []
-  mapM_ (print . findPos a 1 1) pairs 
+  mapM_ (print . findPos a 1 1) pairs
   where
     load :: Int -> [(Int, Int)] -> IO [(Int, Int)]
     load 0 acc = pure $ reverse acc
     load n acc = do
       pairsStr <- getLine
       let pair = helper (map read (words pairsStr)) :: (Int, Int)
-      print pair
       load (n - 1) (pair : acc)
     findPos :: [Int] -> Int -> Int -> (Int, Int) -> Int
     findPos [] _ _ _ = -1
