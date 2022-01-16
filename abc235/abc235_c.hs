e71ce4b7bc42194d6bb2695468e66f3b3179c474
chendianbuji <chendianbuji@gmail.com>
2022-01-17 05:38:44 +0800

    update abc235_c

--- abc235/abc235_c.hs
+++ abc235/abc235_c.hs
@@ -2,6 +2,7 @@ import Control.Monad (replicateM)
 import Data.Char (isSpace)
 import Data.List (unfoldr, concatMap)
 import Data.Maybe (fromJust, fromMaybe)
+import Data.Bifunctor (second)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.IntMap.Strict as IM
 import qualified Data.HashMap.Strict as HM
@@ -49,10 +50,13 @@ main = do
   let pairsMap = makeIndices a
   mapM_ (print . findPos pairsMap) pairs
   where
-    findPos :: HM.HashMap (Int, Int) Int -> (Int, Int) -> Int
-    findPos indices (xi, ki) = HM.lookupDefault (-1) (xi, ki) indices
-    makeIndices :: [ Int ] -> HM.HashMap (Int, Int) Int
-    makeIndices = HM.fromList . fst . foldr (\(idx, cur) (acc1, acc2) ->
-      ( ((cur, IM.findWithDefault 0 cur acc2 + 1), idx) : acc1
-      , IM.insert cur (IM.findWithDefault 0 cur acc2 + 1) acc2))
-      ([], IM.empty :: IM.IntMap Int) . reverse . zip [ 1.. ]
+    findPos :: IM.IntMap (IM.IntMap Int)  -> (Int, Int) -> Int
+    findPos indices (xi, ki) = case IM.lookup xi indices of
+      Just indices2 -> IM.findWithDefault (-1) ki indices2
+      Nothing -> -1
+    makeIndices :: [ Int ] -> IM.IntMap (IM.IntMap Int) 
+    makeIndices a = IM.map (IM.fromList . rzip) (IM.fromListWith (<>) . map (second (pure :: Int -> [ Int ])) . zip a $ [ 1.. ])
+    rzip :: [ Int ] -> [(Int, Int)]
+    rzip xs = zip [l, pred l .. 1] xs
+      where
+        l = length xs
