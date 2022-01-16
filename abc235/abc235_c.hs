6f61461b1ff2de6a1ba8ba6ee20479956d794515
chendianbuji <chendianbuji@gmail.com>
2022-01-17 02:56:19 +0800

    update abc235_c

--- abc235/abc235_c.hs
+++ abc235/abc235_c.hs
@@ -2,6 +2,7 @@ import Control.Monad (replicateM)
 import Data.Maybe (fromJust, fromMaybe)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.IntMap.Strict as IM
+import qualified Data.HashMap.Strict as HM
 
 tuplify2 :: [Int] -> (Int, Int)
 tuplify2 [x, y] = (x, y)
@@ -38,20 +39,17 @@ main = do
   a <- readIntList
   pairs <- readIntTupleListN q
   let pairsMap = makeIndices a
-  print pairsMap
-  print pairs
   mapM_ (print . findPos pairsMap) pairs
   where
-    findPos :: IM.IntMap (IM.IntMap Int) -> (Int, Int) -> Int
-    findPos indices (xi, ki) = case IM.lookup xi indices of
-      Just indices2 -> fromMaybe (-1) (IM.lookup ki indices2)
-      Nothing -> -1
-    makeIndices :: [ Int ] -> IM.IntMap (IM.IntMap Int)
-    makeIndices = foldr (\(idx, cur) acc ->
-      IM.insertWith IM.union cur (makeInsert acc cur idx) acc
-      ) IM.empty . reverse . zip [ 1.. ]
+    findPos :: HM.HashMap (Int, Int) Int -> (Int, Int) -> Int
+    findPos indices (xi, ki) = fromMaybe (-1) ( HM.lookup (xi, ki) indices)
+    makeIndices :: [ Int ] -> HM.HashMap (Int, Int) Int
+    makeIndices = fst . foldr (\(idx, cur) (acc1, acc2) ->
+      ( HM.insert (cur, lookup2 acc2 cur) idx acc1 , makeInsert2 acc2 cur)) (HM.empty, IM.empty :: IM.IntMap Int) . reverse . zip [ 1.. ]
       where
-        makeInsert :: IM.IntMap (IM.IntMap Int) -> Int -> Int -> IM.IntMap Int
-        makeInsert ids k i = case IM.lookup k ids of
-          Just old -> IM.singleton (IM.size old + 1) i
-          Nothing -> IM.singleton 1 i
+        lookup2 :: IM.IntMap Int -> Int -> Int
+        lookup2 ids k = case IM.lookup k ids of
+          Just old -> old + 1
+          Nothing -> 1
+        makeInsert2 :: IM.IntMap Int -> Int -> IM.IntMap Int
+        makeInsert2 ids k = IM.insert k (lookup2 ids k) ids
